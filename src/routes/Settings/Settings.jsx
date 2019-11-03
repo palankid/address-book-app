725f9c10d28306824af0199f33b84ae9a73a705c
Palanki <palankid@emea.roche.com>
2019-11-03 13:21:01 +0100

    Added styles to settings page

--- src/routes/Settings/Settings.jsx
+++ src/routes/Settings/Settings.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Dropdown } from 'semantic-ui-react';
+import {Container, Dropdown, Icon} from 'semantic-ui-react';
+import { useHistory } from 'react-router-dom';
 
 import nationalities from './nationalities.config';
 import { selectNationality } from './reducer';
+import {routeNames} from "../../config/routes.config";
 
 const Settings = () => {
+  const routerHistory = useHistory();
   const dispatch = useDispatch();
   const nationality = useSelector(
     state => state.settings.nationality
@@ -15,19 +18,45 @@ const Settings = () => {
     dispatch(selectNationality(value));
   };
 
-  console.log(nationality);
+  const onBackClick = () => {
+    routerHistory.push(routeNames.root);
+  };
 
   return (
-    <div className="app-settings">
-      <Dropdown
-        fluid
-        selection
-        placeholder="Select Nationality"
-        value={nationality}
-        options={nationalities}
-        onChange={onChangeNationality}
-      />
-    </div>
+    <Container className="settings">
+      <div className="settings__row">
+        <div className="settings__back-link">
+          <Icon
+            link
+            name="arrow left"
+            size="big"
+            onClick={onBackClick}a
+          />
+        </div>
+        <Icon
+          name="setting"
+          size="big"
+          onClick={onBackClick}
+        />
+        <span className="settings__title">
+          Settings
+        </span>
+      </div>
+      <div className="settings__row">
+        <span className="settings__row-title">
+          Nationality
+        </span>
+        <Dropdown
+          className="settings__dropdown"
+          fluid
+          selection
+          placeholder="Select Nationality"
+          value={nationality}
+          options={nationalities}
+          onChange={onChangeNationality}
+        />
+      </div>
+    </Container>
   );
 };
 
